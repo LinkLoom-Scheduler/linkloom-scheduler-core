da15a8cf54d473310e576902f900a2c16a55da4f
Smakouz Illea <i.smakouz@roxydesk.com>
2026-01-14 17:42:07 +0200

    init commit

---
+++ src/main/java/linkloom/Main.java
@@ -0,0 +1,11 @@
+package linkloom;
+
+public class Main {
+    static void main() {
+        IO.println("Hello and welcome to LinkLoom Scheduler Core!");
+
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+}
